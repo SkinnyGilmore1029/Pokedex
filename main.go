d7d683ff83f092204dec76925e0ea6ad26e27eac
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-07-24 07:29:10 -0500

    completed cleanInput

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func cleanInput(text string) []string {
 	var cleaned []string
+	noWhitespace := strings.TrimSpace(text)
+	lowered := strings.ToLower(noWhitespace)
+	splitWords := strings.Fields(lowered)
+	cleaned = splitWords
 
 	return cleaned
 }
@@ -14,3 +19,11 @@ func main() {
 	fmt.Println("Hello, World!")
 
 }
+
+/*
+You'll want to use functions from Go's strings package. Some helpful ones to look into:
+
+strings.TrimSpace() - removes leading/trailing whitespace
+strings.Fields() - splits on whitespace and handles multiple spaces nicely
+strings.ToLower() - converts to lowercase
+*/
